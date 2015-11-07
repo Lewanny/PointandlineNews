e10f4b2092bed7e3e2093e054c83e74d3f55f47a
李宏远 <lihongyuan@zzzzjj.local>
2015-11-07 08:48:30 +0800

    Initial Commit

---
+++ Maximal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maximal
+//
+//  Created by 李宏远 on 15/11/7.
+//  Copyright © 2015年 李宏远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maximal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Maximal
+//
+//  Created by 李宏远 on 15/11/7.
+//  Copyright © 2015年 李宏远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
